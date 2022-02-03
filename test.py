6edb20599307d8526895e1c03b3b12a2b6cab74b
Flitz <joaovitorbprs@gmail.com>
2022-02-03 18:42:48 -0300

    create test.py
    trying to work out selenium

---
+++ test.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+sala = driver.find_element_by_name('raf664648e7d14555898ed0b8c7b4b3a4')
+ano = driver.find_element_by_name('re7c0319c7c1a41588e129a98785252ca')
+nome = driver.find_element_by_class('office-form-question-textbox office-form-textfield-input form-control office-form-theme-focus-border border-no-radius')
+
+print(sala)
+print(ano)
+print(nome)
+
+
