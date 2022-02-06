936bb22e6d73e603ccc1c04980e2470bc960af5d
Flitz <joaovitorbprs@gmail.com>
2022-02-06 15:26:04 -0300

    final
    agora so falta botar as informaçoes de cada um

--- jv.py
+++ jv.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import time
+import smtplib 
+from email.message import EmailMessage
 
 driver = webdriver.Chrome()
 driver.get(
-    'https://forms.office.com/pages/responsepage.aspx?id=QhQEvrbz4UuFCeypyBdSjx4yJ1StPPpItnAoFIkziDJUQ09OMUFHNkRZRzdTOElNVlZCVkRaR1BYQSQlQCNjPTEu'
+    '.'
     ) # Abre o formulario
 
 time.sleep(2) # Tempo pra eu ver o que está acontecendo
@@ -12,15 +14,43 @@ aluno = driver.find_element_by_xpath('') # x-path do input do nome do aluno
 ano_2 = driver.find_element_by_xpath('') # x-path da alternativa do ano
 sala_A = driver.find_element_by_xpath('') # x-path da alternativa da sala
 submit = driver.find_element_by_xpath('') # x-path do botao de submeter
+confirmar = driver.find_element_by_css_selector('') # a classe de confirmação de enviado
+print(confirmar.text)
 
-
-aluno.send_keys('João Vítor Benjamim Pires')
+aluno.send_keys('.')
 
 sala_A.click()
 ano_2.click()
 submit.click()
 
-time.sleep(2)
+time.sleep(1)
+
+############ CONFIRMAÇÃO POR EMAIL ############
+
+username = "." 
+password = "." 
+
+msg = EmailMessage()
+msg['From'] = username # de quem é
+msg['To'] = '.' # Pra quem é
+
+if(confirmar.text) == "O texto que aparece qnd vc manda":
+    print("td certin") 
+    msg['Subject'] = 'Checklist Diário' # Assunto Email
+    msg.set_content('confirmar.text') # a mensagem
+    #
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp: 
+        smtp.login(username, password) # Login usando o email
+        smtp.send_message(msg) # Manda a mensagem
+else:
+    print('eRrOr')
+    # manda o email de erro tb
+    msg['Subject'] = 'Cagou'
+    msg.set_content('Vai ter que ser manual msm') 
+    #
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp: 
+        smtp.login(username, password) 
+        smtp.send_message(msg) 
 
 
 driver.quit()
--- triz.py
+++ triz.py
@@ -1,21 +1,56 @@
 from selenium import webdriver
 import time
+import smtplib 
+from email.message import EmailMessage
 
 driver = webdriver.Chrome()
 driver.get(
-    'https://forms.office.com/pages/responsepage.aspx?id=QhQEvrbz4UuFCeypyBdSjx4yJ1StPPpItnAoFIkziDJUQ09OMUFHNkRZRzdTOElNVlZCVkRaR1BYQSQlQCNjPTEu'
+    '.'
     ) # Abre o formulario
 
 time.sleep(2) # Tempo pra eu ver o que está acontecendo
 
 aluno = driver.find_element_by_xpath('') # x-path do input do nome do aluno
-ano = driver.find_element_by_xpath('') # x-path do input do ano
-sala = driver.find_element_by_xpath('') # x-path do input da sala
+ano_2 = driver.find_element_by_xpath('') # x-path da alternativa do ano
+sala_A = driver.find_element_by_xpath('') # x-path da alternativa da sala
+submit = driver.find_element_by_xpath('') # x-path do botao de submeter
+confirmar = driver.find_element_by_css_selector('') # a classe de confirmação de enviado
+print(confirmar.text)
 
-aluno.send_keys('Beatriz Benjamim Pires')
+aluno.send_keys('.')
 
+sala_A.click()
+ano_2.click()
+submit.click()
 
-time.sleep(2)
+time.sleep(1)
+
+############ EMAIL ############
+
+username = "." 
+password = "." 
+
+msg = EmailMessage()
+msg['From'] = username # de quem é
+msg['To'] = '.' # Pra quem é
+
+if(confirmar.text) == "O texto que aparece qnd vc manda":
+    print("td certin") 
+    msg['Subject'] = 'Checklist Diário' # Assunto Email
+    msg.set_content('confirmar.text') # a mensagem
+    #
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp: 
+        smtp.login(username, password) # Login usando o email
+        smtp.send_message(msg) # Manda a mensagem
+else:
+    print('eRrOr')
+    # manda o email de erro tb
+    msg['Subject'] = 'Cagou'
+    msg.set_content('Vai ter que ser manual msm') 
+    #
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp: 
+        smtp.login(username, password) 
+        smtp.send_message(msg) 
 
 
 driver.quit()
